0cd5014f89bdc5e8eb35375227ee7258cdca4a3b
OKAUEND <def1f23sbb@gmail.com>
2020-05-15 15:33:39 +0900

    New - viewsにベースとなるvueファイルを新規作成

---
+++ src/views/Footer.vue
@@ -0,0 +1,11 @@
+<!-- 現状仮置 -->
+<template>
+  <div>
+    <!-- フッター -->
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
---
+++ src/views/Pages.vue
@@ -0,0 +1,11 @@
+<!-- 現状仮置 -->
+<template>
+  <div>
+    本体表示部分
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
